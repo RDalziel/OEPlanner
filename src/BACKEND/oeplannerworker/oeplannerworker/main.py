35c8bed8f8cba5300e1116d4c519691c62719f06
Richard Dalziel <RichardDalziel@outlook.com>
2023-08-15 23:04:39 +0100

    Add global exception handler to the worker

--- src/BACKEND/oeplannerworker/oeplannerworker/main.py
+++ src/BACKEND/oeplannerworker/oeplannerworker/main.py
@@ -1,8 +1,18 @@
 from oeplannertasks import celery_app
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import traceback
 
 app = FastAPI()
 celery = celery_app.app
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    stack_trace = traceback.format_exc()
+    return JSONResponse(
+        status_code=500,
+        content={"message": str(exc), "stack_trace": stack_trace}
+    )
 
 
 @app.on_event("startup")
